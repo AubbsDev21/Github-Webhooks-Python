98cdd94d7dc904e66340e4fbc35dbc3c2ed57e9e
AubbsDev21 <bodyaubre@gmail.com>
2025-10-04 12:02:27 -0400

    send code over for clean branch

--- get_payload.py
+++ get_payload.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import json
 from logic.get_payload import GetPayload
 from models.get_payload_model import GithubRawPayload
 
@@ -13,6 +14,13 @@ def event():
        result = instance.payload_validation(payload)
 
        print("Message: " + result)
+
+    @app.put("/parse_data")
+    async def handler(request: Request):
+        payload = await request.json()
+        instance = GetPayload(json.dumps(payload))
+        new_payload = instance.payload_parser()
+        return new_payload 
 
 if __name__ == "__main__":
     event()
--- logic/get_payload.py
+++ logic/get_payload.py
@@ -1,6 +1,6 @@
+from pydantic import ValidationError
 import json
 from models.get_payload_model import GithubRawPayload
-from pydantic import ValidationError
 import logging
 
 
@@ -23,21 +23,54 @@ class GetPayload:
 
         except ValidationError as e:
             logging.error(f"Validation Error: {e}")
-            raise  
+            raise 
 
     def payload_parser(self) -> dict:
-          data = {}
+        data = {}
         try:
           payload = json.loads(self.payload)
           # Navigating nested structures
-          #engineering_employees = data["departments"][0]["employees"]
-          hook_id = payload["hook"]["id"]
-          update_time = payload["hook"]["updated_at"]
-          create_time = payload["hook"]["created_at"]
+          hook = payload["hook"]
+          hook_id = hook["id"]
+          update_time = hook["updated_at"]
+          create_time = hook["created_at"]
 
-          full_name = payload["repository"]["full_name"]
-          owner_login = payload["repository"]["owner"]["login"]
-          owner_type = payload["repository"]["owner"]["type"]
-          comment_url = payload["repository"]["comments_url"]
+          repo = payload["repository"]
+          owner = repo["owner"]
+          full_name = repo["full_name"]
+          owner_login = owner["login"]
+          owner_type = owner["type"]
+          comment_url = repo["comments_url"]
+          issue_comments_url = repo["issue_comment_url"]
+          created_at = repo["created_at"]
+          updated_at = repo["updated_at"]
+          language = repo["language"]
+          
+          data = {
+              "hook": {
+                  "id": hook_id,
+                  "update_time": update_time,
+                  "create_time": create_time
+              },
+              "repository": {
+                  "full_name": full_name,
+                  "owner": {
+                      "login": owner_login,
+                      "type": owner_type
+                  },
+                  "comments_url": comment_url,
+                  "issue_comments_url": issue_comments_url,
+                  "created_at": created_at,
+                  "updated_at": updated_at,
+                  "language": language
+              }
+          }
+
+          parse_paylaod = json.dumps(data, indent=2)
+          return parse_paylaod
+        
+        except TypeError as err:
+            logging.error(f"Type Error: {err}")
+            raise
           
         
